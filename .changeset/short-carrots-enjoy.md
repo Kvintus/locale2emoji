9be42bf6538095d1b2db7447d48a0ec896e44098
Jákob Rolík <daidaloskvintus@gmail.com>
2024-08-30 02:08:01 +0200

    chore: stopped generating source maps

---
+++ .changeset/short-carrots-enjoy.md
@@ -0,0 +1,5 @@
+---
+"locale2emoji": minor
+---
+
+Removed inclusion of source maps in the build. This greatly reduces the bundle size
--- tsup.config.ts
+++ tsup.config.ts
@@ -3,8 +3,9 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
     entry: ['src/index.ts'],
     splitting: true,
-    sourcemap: true,
+    sourcemap: false,
     format: ['cjs', 'esm'],
     dts: true,
     clean: true,
+    minify: true,
 })
